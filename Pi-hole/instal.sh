cdb228422201b5e3555c1ad9130591b902e96bb9
Piotr <piotr.kacprzak@likims.com>
2024-03-18 13:39:29 +0100

    change docker instaaltion

--- Pi-hole/instal.sh
+++ Pi-hole/instal.sh
@@ -33,21 +33,19 @@ sudo systemctl restart nginx
 
 # install docker 
 echo "Docker instalation"
-# Downloading and adding Docker GPG key
-curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo apt-key add -
-
-# Adding Docker Engine repository for armhf architecture (Raspberry Pi)
-echo "deb [arch=armhf] https://download.docker.com/linux/raspbian buster stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Updating APT package index and installing Docker Engine
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Adding current user to docker group to avoid needing sudo when running Docker commands
-sudo usermod -aG docker $USER
-
-# Checking installed Docker version
-docker --version
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 # Start docker
 sudo systemctl start docker
