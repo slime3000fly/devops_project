e620016d44f241fedb9b1d2225b7a4d5b30319bf
Piotr <piotr.kacprzak@likims.com>
2024-03-18 14:05:39 +0100

    -y

--- Pi-hole/instal.sh
+++ Pi-hole/instal.sh
@@ -48,7 +48,7 @@ echo \
 sudo apt-get update
 
 # Install docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Start docker
 sudo systemctl start docker
